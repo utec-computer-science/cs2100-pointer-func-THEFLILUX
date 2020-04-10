a44a0df383c221534da15aa65f8dcb77faf2dc3a
Jorge Ernesto Nicho Galagarza <jorge.nicho@utec.edu.pe>
2020-04-10 02:00:04 -0500

    Functions Completed

---
+++ cmake-build-debug/CMakeFiles/cs2100-pointer-func-THEFLILUX.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs2100-pointer-func-THEFLILUX.dir/main.cpp.o"
+  "cs2100-pointer-func-THEFLILUX"
+  "cs2100-pointer-func-THEFLILUX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs2100-pointer-func-THEFLILUX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
